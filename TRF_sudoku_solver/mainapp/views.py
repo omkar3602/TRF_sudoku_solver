89004edd1f8d9f00315de21d26042e3f04d1c592
omkar3602 <73201152+omkar3602@users.noreply.github.com>
2022-01-26 00:27:41 +0530

    Add solver files

--- TRF_sudoku_solver/mainapp/views.py
+++ TRF_sudoku_solver/mainapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from . import sudokusolver
 # Create your views here.
 def home(request):
+    return render(request,'index.html')
+
+def api(request):
+
+    sudokusolver.call_solver()
+
     return render(request,'index.html')
