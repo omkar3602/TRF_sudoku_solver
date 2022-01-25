2c6a63d102ed2df8ea02ceb9f663b3f294ba02fa
omkar3602 <73201152+omkar3602@users.noreply.github.com>
2022-01-26 03:11:37 +0530

    Modify project settings to deploy

--- TRF_sudoku_solver/TRF_sudoku_solver/settings.py
+++ TRF_sudoku_solver/TRF_sudoku_solver/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pl0j#hk21w(6#8y#pi%+20=2y2ot!t%1b$#_@db)&ux8y&6-0p'
+SECRET_KEY = os.environ.get("SECRET_KEY", BASE_DIR)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -133,3 +134,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
