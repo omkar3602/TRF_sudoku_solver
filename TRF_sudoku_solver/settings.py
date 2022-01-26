7fe1383a8e9467c71e139733d4be8cfc2246a91b
omkar3602 <73201152+omkar3602@users.noreply.github.com>
2022-01-26 12:48:30 +0530

    Add ALLOWED_HOSTS

--- TRF_sudoku_solver/settings.py
+++ TRF_sudoku_solver/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", BASE_DIR)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'sudoku-solver-3602.herokuapp']
 
 
 # Application definition
